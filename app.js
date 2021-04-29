1d953d02e52bb785b2a3460ba373b18b3836cc37
Thiago Rocha Moreira Paiva <pste13.informatica@crecisp.gov.br>
2021-04-29 09:52:02 -0300

    Removing tiledesk integration, that is now in refactored-guacamole

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const functions = require('./lib/functions.js')
-const { TiledeskChatbotClient } = require('@tiledesk/tiledesk-chatbot-client');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.post('/thechatbot/v1/answerme', functions.answerFunction)
 app.get('/thechatbot/v1/ping', functions.iAmOk)
-app.post('/thechatbot/v1/answerme/tiledesk', (req, res) => {
-  const tdclient = 
-    new TiledeskChatbotClient({request: req, response: res})
-  console.log('Pergunta recebida: ' + tdclient.text)
-  res.status(200).send({"success":true});
-  let msg = {
-    text: functions.processQuestion(tdclient.text,false)
-  }
-  tdclient.sendMessage(msg)
-})
-
-// Alternative chatbot endpoint with raw http call
 
 app.listen(29005, () => {
   console.log('server started');
